f64c1647fe31afef6f819e83cec5dfbdbf89eee9
amipy <46629183+amipy@users.noreply.github.com>
2021-02-07 16:17:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import psycopg2
 import json
 import time
 import math
+import datetime
 
 from Command import Command
 
 with open("timexe_config.json") as conf_file:
     config=json.load(conf_file)
-print(config)
+#print(config)
 dbase=psycopg2.connect(host=config["host"], database=config["db"], user=config["user"], password=config["pass"])
 
 cursor=dbase.cursor()
@@ -27,9 +28,21 @@ min_interval=intervals[0]
 if len(intervals)>1:
     min_interval=math.gcd(*intervals)
 
-print(min_interval)
+minty=commands[0]._startTime
+min_time=commands[0]._startTimeDT
+for i in commands:
+    if i._startTimeDT<=min_time:
+        min_time=i._startTimeDT
+        minty=i._startTime
+
+#print(min_interval)
 running = True
-print("The program is running...")
+dtnow = datetime.datetime.now()
+while dtnow.replace(year=dtnow.year, month=dtnow.month, day=dtnow.day)<min_time:
+    dtnow = datetime.datetime.now()
+    print(min_time-dtnow.replace(year=dtnow.year, month=dtnow.month, day=dtnow.day, microsecond=0))
+    time.sleep(1)
+print("Executing.")
 while running:
     finish = True
     for c in commands:
@@ -42,6 +55,3 @@ while running:
     time.sleep(min_interval/1000)
 
 print("Finished")
-
-
-
